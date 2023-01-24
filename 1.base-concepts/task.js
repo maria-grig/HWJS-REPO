8c4f362addc83d042d9437fc0e6813b52381804e
MariaG <MGrigorenko@bellintegrator.com>
2023-01-24 22:18:51 +0300

    refactor

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -3,16 +3,15 @@
 // Задача № 1.1
 
 function solveEquation(a, b, c) {
-  let arr;
+  let radical = [];
 
-  arr = [];
   let d = (b**2-4*a*c);
   if (d > 0){
-    arr.push((-b + Math.sqrt(d) )/(2*a));
-    arr.push((-b - Math.sqrt(d) )/(2*a));
+    radical.push((-b + Math.sqrt(d) )/(2*a));
+    radical.push((-b - Math.sqrt(d) )/(2*a));
   }
   if (d === 0){
-    arr.push(-b/(2*a));
+    radical.push(-b/(2*a));
   }
-  return arr;
+  return radical;
 }
