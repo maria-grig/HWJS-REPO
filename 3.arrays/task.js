1e355e7e79ce26e9270a6947d87f8b13a9f5cecd
MariaG <MGrigorenko@bellintegrator.com>
2023-02-25 18:44:01 +0300

    Arrays: task 1 & 2

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,17 @@
+"use strict";
+// Задача 1 - функция сравнивает значения двух массивов
 function compareArrays(arr1, arr2) {
-  
+    return arr1.length === arr2.length && arr1.every((item, index) => item === arr2[index]);
 }
 
+// Задача 2 - функция возвращает среднее значение возраста пользователей одного пола
 function getUsersNamesInAgeRange(users, gender) {
-  
-}
+    if (users.length == 0) {
+        return 0;
+    }
+    if (gender === "мужской" || gender === "женский") {
+        let gendersArr = users.filter(user => user.gender === gender).map(user => user.age);
+        return gendersArr.reduce((acc, item) => acc + item) / gendersArr.length;
+
+    }
+        return 0;
